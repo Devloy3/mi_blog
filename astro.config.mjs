c1ae0b8bde0bf08077cf5857072abe48499586eb
Eloy Dominguez <eloydominguez3@gmail.com>
2026-03-11 21:10:01 +0100

    configuracion

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: 'https://devloy3.github.io',
+	base: "/mi_blog",
 	integrations: [mdx(), sitemap()],
 });
